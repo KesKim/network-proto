0b81f9cd2b3cfbcb968278bd90d91eb02ef55029
Kimmo Keskinen <keskinenkimmo@gmail.com>
2017-04-26 12:57:58 +0300

    (DynamicPlayerView): TEMPORARY FIX client-side nullref due to PlayerInfo not being in sync and not having its members set correctly.

--- Assets/DiscoveryTest/DynamicPlayerView.cs
+++ Assets/DiscoveryTest/DynamicPlayerView.cs
@@ -49,6 +49,6 @@ public class DynamicPlayerView : MonoBehaviour
 		GameObject item = (GameObject)Instantiate(prefab, holderParent);
 
         Text text = item.GetComponentInChildren<Text>();
-		text.text = "Player " + _player.uniquePlayerId + " - COLOR - " + _player.connection.address;
+		text.text = "Player " + _player.uniquePlayerId + " - COLOR - " + ((_player.connection != null) ?_player.connection.address : "no conn");
     }
 }
